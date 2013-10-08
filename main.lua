c151e6abccb0ff6378f2b8a4d06299de55f19cc5
Patrick Abbs <patricka@headspring.com>
2013-10-07 23:27:55 -0500

    cloudz

--- main.lua
+++ main.lua
@@ -1,7 +1,4 @@
 
--- tutorial #1
--- the 3 golden love function, lua tables, keyboard input, draw circle
-
 function love.load()
  hero = {} -- new table for the hero
  hero.x = 300    -- x,y coordinates of the hero
@@ -19,11 +16,7 @@ function love.load()
 
  enemies = {}
 
- enemy = {}
- enemy.x = 1000
- enemy.y = 450
- enemy.speed = -100
- table.insert(enemies, enemy)
+ clouds = {}
 end
 
 function love.update(dt)
@@ -34,10 +27,15 @@ function love.update(dt)
 	end
  end
 
- --enemy.x = enemy.x + enemy.speed*dt
-
  for i,enemy in ipairs(enemies) do
     enemy.x = enemy.x + enemy.speed*dt
+	if enemy.x > hero.x and enemy.x < hero.x + 100 and hero.jumpingNow == false then
+	  hero.jump()
+	end
+ end
+
+ for i,cloud in ipairs(clouds) do
+    cloud.x = cloud.x + cloud.speed*dt
  end
 
  if hero.jumpingNow == true then
@@ -51,14 +49,23 @@ function love.update(dt)
  end
 
  if math.random() < .001 then
-    if hero.jumpingNow == false then
-      hero.jump()
-	end
     enemy = {}
     enemy.x = 1000
     enemy.y = 450
+	enemy.width = 30
+	enemy.height = 15
     enemy.speed = -200
 	table.insert(enemies, enemy)
+ end
+
+ if math.random() < .0005 then
+   cloud = {}
+   cloud.x = 1000
+   cloud.y = math.random(350)
+   cloud.width = math.random(55) + 35
+   cloud.height = math.random(45) + 15
+   cloud.speed = -50
+   table.insert(clouds, cloud)
  end
 
 end
@@ -68,19 +75,24 @@ function love.draw()
  love.graphics.setColor(0,255,0,255)
  love.graphics.rectangle("fill", 0,465,800,150)
 
+ --clouds
+ love.graphics.setColor(20, 100, 200, 255)
+ for i,cloud in ipairs(clouds) do
+    love.graphics.rectangle("fill", cloud.x, cloud.y, cloud.width, cloud.height)
+ end
+
  -- let's draw our hero
+ love.graphics.setColor(0, 255, 0, 255)
  if heroColor == "red" then
    love.graphics.setColor(255,0,0,255)
  end
 
  love.graphics.rectangle("fill", hero.x,hero.y, 30,15)
 
- --enemy
+ --enemies
  love.graphics.setColor(255,0,0,255)
- love.graphics.rectangle("fill", enemy.x, enemy.y, 30,15)
-
- for i,v in ipairs(enemies) do
-    love.graphics.rectangle("fill", v.x, v.y, 30, 15)
+ for i,enemy in ipairs(enemies) do
+    love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
  end
 end
 

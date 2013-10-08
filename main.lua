b4fca8689c9e14ddc0d032819c2c27eb0c3b86c2
Patrick Abbs <patricka@headspring.com>
2013-10-07 22:45:35 -0500

    hero jumps and responds to collisions

--- main.lua
+++ main.lua
@@ -6,7 +6,16 @@ function love.load()
  hero = {} -- new table for the hero
  hero.x = 300    -- x,y coordinates of the hero
  hero.y = 450
- hero.speed = 100
+ hero.jumpSpeed = .3
+ hero.jumpNow = 0
+ hero.jumpingNow = false
+ function hero.jump()
+   hero.y = hero.y - hero.jumpSpeed
+   hero.jumpingNow = true
+ end
+ function hero.move()
+   hero.y = hero.y - hero.jumpSpeed
+ end
 
  enemies = {}
 
@@ -18,33 +27,37 @@ function love.load()
 end
 
 function love.update(dt)
- --if love.keyboard.isDown("left") then
- --  hero.x = hero.x - hero.speed*dt
- --elseif love.keyboard.isDown("right") then
- --  hero.x = hero.x + hero.speed*dt
- --end
- --if love.keyboard.isDown("down") then
- --  hero.y = hero.y + hero.speed*dt
- --elseif love.keyboard.isDown("up") then
- --  hero.y = hero.y - hero.speed*dt
- --end
  heroColor = "green"
- --if hero.y < enemy.y+15 and hero.y > enemy.y-15 and hero.x < enemy.x+30 and hero.x > enemy.x-30 then
- --  heroColor = "red"
- --end
-
+ for i,enemy in ipairs(enemies) do
+    if hero.y < enemy.y+15 and hero.y > enemy.y-15 and hero.x < enemy.x+30 and hero.x > enemy.x-30 then
+	  heroColor = "red"
+	end
+ end
 
  --enemy.x = enemy.x + enemy.speed*dt
 
- for i,v in ipairs(enemies) do
-    v.x = v.x + v.speed*dt
+ for i,enemy in ipairs(enemies) do
+    enemy.x = enemy.x + enemy.speed*dt
+ end
+
+ if hero.jumpingNow == true then
+    hero.jumpSpeed = hero.jumpSpeed - .001
+    hero.move()
+	if hero.y > 450 then
+	  hero.y = 450
+	  hero.jumpingNow = false
+	  hero.jumpSpeed = .3
+	end
  end
 
  if math.random() < .001 then
+    if hero.jumpingNow == false then
+      hero.jump()
+	end
     enemy = {}
     enemy.x = 1000
     enemy.y = 450
-    enemy.speed = -100
+    enemy.speed = -200
 	table.insert(enemies, enemy)
  end
 
@@ -59,7 +72,7 @@ function love.draw()
  if heroColor == "red" then
    love.graphics.setColor(255,0,0,255)
  end
- --elseif heroColor == "red" then
+
  love.graphics.rectangle("fill", hero.x,hero.y, 30,15)
 
  --enemy
